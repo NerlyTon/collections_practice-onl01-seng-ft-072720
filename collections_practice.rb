9e6779daa3ecaf9606dcef76bd043899e2eecfc9
Nerly Ton <nerlyton@gmail.com>
2020-07-31 23:24:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(n)
+  n.sort do |a, b|
+    a <=> b
+  end
+end
+  
+def sort_array_desc(n)
+  n.sort do |a, b|
+    b <=> a 
+  end
+end
 
+def sort_array_char_count(n)
+  n.sort do |a, b|
+    a.length <=> b.length 
+  end
+end
+
+def swap_elements(n)
+  n[1], n[2] = n[2], n[1] 
+  n
+end
+
+def reverse_array(n)
+  n.reverse
+end
+
+def kesha_maker(array)
+  
